97495ba45da158ca3b18ab80abbe0bf730f6ac57
dzmitryrazhkou <dzmitryrazhkou@gmail.com>
2022-09-20 00:56:21 -0700

    pages

--- src/main/java/com/qa/pages/SearchPage.java
+++ src/main/java/com/qa/pages/SearchPage.java
@@ -1,10 +1,8 @@
 package com.qa.pages;
 
 import org.apache.log4j.Logger;
-import org.openqa.selenium.By;
-import org.openqa.selenium.TimeoutException;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -28,7 +26,7 @@ public class SearchPage {
         return driver.findElement(searchProductValidationLocator);
     }
 
-    public String searchValidator() {
+    public String searchValidator() throws InterruptedException {
         try {
             log.info("Product of the Search");
             System.out.println(getSearchProductValidation().getText());
@@ -57,11 +55,87 @@ public class SearchPage {
         }
     }
 
-    public String getProductSorted() {
+    public String getProductSorted() throws InterruptedException {
         By productSortedLocator = By.cssSelector("div[class='products wrapper grid products-grid']:nth-child(2) li:nth-of-type(2) div div div span span span span");
         wait.until(ExpectedConditions.presenceOfElementLocated(productSortedLocator));
         String productPrice = driver.findElement(productSortedLocator).getText();
         System.out.println(" =====> " +productPrice+ " <===== ");
         return productPrice;
     }
+
+    private WebElement getSize(){
+        By sizeLocator = By.cssSelector("div[class='products wrapper grid products-grid']:nth-child(2) li:nth-of-type(2) div div[class='swatch-attribute size'] div:nth-of-type(4)");
+        wait.until(ExpectedConditions.presenceOfElementLocated(sizeLocator));
+        return driver.findElement(sizeLocator);
+    }
+
+    private WebElement getColor(){
+        By colorLocator = By.cssSelector("div[class='products wrapper grid products-grid']:nth-child(2) li:nth-of-type(2) div div[class='swatch-attribute color'] div:nth-of-type(2)");
+        wait.until(ExpectedConditions.presenceOfElementLocated(colorLocator));
+        return driver.findElement(colorLocator);
+    }
+
+    private WebElement approachToAddToCartButton() {
+        By fullElementLocator = By.cssSelector("div[class='products wrapper grid products-grid']:nth-child(2) li:nth-of-type(2)");
+        wait.until(ExpectedConditions.presenceOfElementLocated(fullElementLocator));
+        return driver.findElement(fullElementLocator);
+    }
+
+    private WebElement getAddToCartButton() {
+        By addToCartLocator = By.cssSelector("div[class='products wrapper grid products-grid']:nth-child(2) li:nth-of-type(2) div div button span");
+        wait.until(ExpectedConditions.presenceOfElementLocated(addToCartLocator));
+        return driver.findElement(addToCartLocator);
+    }
+
+    public void doAddToCart() {
+        log.info("User selects size of the product. ");
+        getSize().click();
+        log.info("User selects color of the product. ");
+        getColor().click();
+
+        Actions act = new Actions(driver);
+        try {
+            log.info("User makes mouse over to `Add To Cart` button. ");
+            act.moveToElement(approachToAddToCartButton()).build().perform();
+        } catch (ElementNotInteractableException y){
+            System.out.println(y);
+        }
+        log.info("User clicks on the `Add To Cart` button. ");
+        getAddToCartButton().click();
+    }
+
+    private WebElement getCart() {
+        By cartLocator = By.cssSelector("a[class='action showcart']");
+        wait.until(ExpectedConditions.presenceOfElementLocated(cartLocator));
+        return driver.findElement(cartLocator);
+    }
+
+    public String getProductPriceInTheCart() {
+        By cartLocator = By.cssSelector("div[class='price-container'] span span span span");
+        wait.until(ExpectedConditions.presenceOfElementLocated(cartLocator));
+        log.info("User validates price from product in the cart");
+        String txt = driver.findElement(cartLocator).getText();
+        System.out.println("=====> " +txt+ " <===== ");
+        return txt;
+    }
+
+    public void doClickOnTheCart(){
+        log.info("User clicks on the cart");
+        getCart().click();
+    }
+
+    private WebElement getProceedToCheckOut() {
+        By proceedToCheckOutLocator = By.cssSelector("#top-cart-btn-checkout");
+        wait.until(ExpectedConditions.presenceOfElementLocated(proceedToCheckOutLocator));
+        return driver.findElement(proceedToCheckOutLocator);
+    }
+
+    public ShippingAddressPage doClickCartAndProceed() {
+        log.info("User clicks on the cart");
+        getCart().click();
+        log.info("User clicks on the proceed to checkout");
+        getProceedToCheckOut().click();
+        return new ShippingAddressPage(driver);
+    }
+
 }
--- src/test/java/com/qa/luma/test/SearchPageTest.java
+++ src/test/java/com/qa/luma/test/SearchPageTest.java
@@ -2,7 +2,9 @@ package com.qa.luma.test;
 
 import com.qa.browser.BrowserFactory;
 import com.qa.enums.Browsers;
+import com.qa.pages.LoginPage;
 import com.qa.pages.MainPage;
+import com.qa.pages.ShippingAddressPage;
 import com.qa.pages.SearchPage;
 import com.qa.utils.ScreenShot;
 import org.openqa.selenium.WebDriver;
@@ -18,10 +20,12 @@ import java.net.MalformedURLException;
 @Listeners(com.qa.luma.listeners.Listeners.class)
 public class SearchPageTest {
     private WebDriver driver;
+    LoginPage loginPage;
+    ShippingAddressPage shippingAddressPage;
 
     @BeforeMethod
     public void startUp() throws MalformedURLException {
-        driver = BrowserFactory.getBrowser(Browsers.CHROME);
+        driver = BrowserFactory.getBrowser(Browsers.CHROME_HEADLESS);
         driver.get("https://magento.softwaretestingboard.com/");
     }
 
@@ -34,7 +38,7 @@ public class SearchPageTest {
     }
 
     @Test(priority = 1)
-    public void searchTest(){
+    public void searchTest() throws InterruptedException {
         String product = "Hoodie";
         String expSearchResult = "Search results for: 'Hoodie'";
 
@@ -45,7 +49,7 @@ public class SearchPageTest {
     }
 
     @Test(priority = 2)
-    public void validateProductSortTest() {
+    public void validateProductSortTest() throws InterruptedException {
         String product = "Hoodie";
         String price = "Price";
         MainPage mainPage = new MainPage(driver);
@@ -54,5 +58,39 @@ public class SearchPageTest {
         String actRes = searchPage.getProductSorted();
         String expRes = "$70.00";
         Assert.assertEquals(expRes, actRes);
+    }
+
+    @Test(priority = 3)
+    public void addToCartTest() {
+        String product = "Hoodie";
+        String price = "Price";
+        String email = "dimagadjilla@gmail.com";
+        String password = "3036057Dr";
+
+        MainPage mainPage = new MainPage(driver);
+        loginPage = mainPage.clickOnSignIn();
+        loginPage.login(email, password);
+        SearchPage searchPage = mainPage.searchProduct(product);
+        searchPage.getSortText(price);
+        searchPage.doAddToCart();
+        searchPage.doClickOnTheCart();
+        Assert.assertEquals(searchPage.getProductPriceInTheCart(), "$48.00");
+    }
+
+    @Test(priority = 4)
+    public void proceedToCheckOutTest() throws InterruptedException {
+        String product = "Hoodie";
+        String price = "Price";
+        String email = "dimagadjilla@gmail.com";
+        String password = "3036057Dr";
+
+        MainPage mainPage = new MainPage(driver);
+        loginPage = mainPage.clickOnSignIn();
+        loginPage.login(email, password);
+        SearchPage searchPage = mainPage.searchProduct(product);
+        searchPage.getSortText(price);
+        searchPage.doAddToCart();
+        shippingAddressPage = searchPage.doClickCartAndProceed();
+        Assert.assertTrue(shippingAddressPage.shippingAddressBreadcrumbDisplayed());
     }
 }
